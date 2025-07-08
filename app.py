6d54012cf29035397cc0e39f0e1fc81c7d81b03d
Viet Nguyen <viet.techlearner@gmail.com>
2025-07-08 10:04:42 -0700

    Add interactive Plotly charts with Streamlit header and checkbox

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # Set a title
 st.title("🚗 Used Cars Dataset Explorer")
@@ -14,3 +15,8 @@ df = load_data()
 # Show the dataframe
 st.write("Here’s a preview of the dataset:")
 st.dataframe(df)
+
+# Add a plotly express box plot of price by vehicle type
+st.write("### Price Distribution by Vehicle Type")
+fig = px.box(df, x="type", y="price", title="Price Distribution by Vehicle Type")
+st.plotly_chart(fig)
